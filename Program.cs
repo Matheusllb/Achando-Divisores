ad72bd448742aab905b32658af622ede03e64c55
Matheusllb <matheusluislopesbatista@gmail.com>
2023-04-04 21:06:43 -0300

    Achando divisores

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CsE23
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //EXERCÍCIO 23
+            Console.Write("Achando os divisores\n\nInforme um valor: ");
+            int N = int.Parse(Console.ReadLine());
+            int divisores;
+            
+            for (int i = 1; i <= N; i++)
+            {
+                if(N % i == 0)
+                    Console.WriteLine("\nDivisores:" + i);
+            }
+            Console.WriteLine("\nFim do Programa!\n");
+        }
+    }
+}
